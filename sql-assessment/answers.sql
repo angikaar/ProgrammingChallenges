6acf50ca209630d774d5a62c35d39e5ba9986818
Angikaar Singh Chana <54721867+angikaar@users.noreply.github.com>
2023-08-30 21:11:49 -0700

    Completed Q2, Q3, half of Q4
    Ran the code for Q2 to get the third-best state revenue, completed first-draft code for Q3, and completed half of Q4. Downloading locally to find which state generated the most conversions for Campaign5

--- sql-assessment/answers.sql
+++ sql-assessment/answers.sql
@@ -14,3 +14,33 @@ FROM website_revenue
 GROUP BY state
 ORDER BY total_revenue DESC
 LIMIT 3;
+-- After running the code locally, the third-best state was "OH", Ohio, with a total revenue of 37577.0.
+
+-- QUESTION 3. Write a query that shows total cost, impressions, clicks, and revenue of each campaign.
+-- Make sure to include the campaign name in the output.
+SELECT
+  ci.name AS camapign_name,
+  SUM(mp.cost) AS total_cost,
+  SUM(mp.impressions) AS total_impressions,
+  SUM(mp.clicks) AS total_clicks,
+  SUM(wr.revenue) AS total_revenue
+FROM marketing_performance mp
+JOIN campaign_info ci ON mp.campaign_id = ci.id
+JOIN
+  website_revenue wr ON mp.campaign_id = wr.campaign_id AND
+  mp.date = wr.date
+GROUP BY ci.name;
+
+-- QUESTION 4. Write a query to get the number of conversions of Campaign5 by state.
+-- Which state generated the most conversions for this campaign?
+SELECT
+  mp.geo AS state,
+  SUM(mp.conversions) AS total_conversions
+FROM marketing_performance mp
+JOIN campaign_info ci ON mp.campaign_id = ci.id
+WHERE ci.name = 'Campaign5'
+GROUP BY mp.geo
+ORDER BY total_conversions DESC;
+
+
+
