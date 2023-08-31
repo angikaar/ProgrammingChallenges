23e09332635ed06c482fa0a48470418f7eca4fe3
Angikaar Singh Chana <54721867+angikaar@users.noreply.github.com>
2023-08-30 20:54:54 -0700

    Completed Q1, half of Q2
    Completed Q1 and started Q2, committing to run code for Q2 to find out revenue for third-best state

--- sql-assessment/answers.sql
+++ sql-assessment/answers.sql
@@ -1,1 +1,16 @@
+-- QUESTION 1. Write a query to get the sum of impressions by day.
+SELECT
+  date,
+  SUM(impressions) AS total_impressions
+FROM marketing_performance
+GROUP BY date;
 
+-- QUESTION 2. Write a query to get the top three revenue-generating states in order of best to worst.
+-- How much revenue did the third best state generate?
+SELECT
+  state,
+  SUM(revenue) AS total_revenue
+FROM website_revenue
+GROUP BY state
+ORDER BY total_revenue DESC
+LIMIT 3;
